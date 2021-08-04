910a00650428746116a87f3427410db80bcf2954
fajriputra <fajriwn27@gmail.com>
2021-08-04 11:47:01 +0700

    belajar req (body, params, query)

--- routes/mahasiswa.js
+++ routes/mahasiswa.js
@@ -2,26 +2,37 @@ const express = require('express');
 const router = express.Router();
 const db = require('../config/mysql');
 
-router.get('/', (req, res, next) => {
-    let sql = "SELECT * FROM mahasiswa";
+router.get('/search', (req, res, next) => {
+    const nim = req.query.nim;
+    let sql = "SELECT * FROM mahasiswa WHERE nim = " + nim;
     db.query(sql, (err, result) => {
         if (err) throw err;
-        res.status(200).json({
-            message: 'Get Method Mahasiswa',
-            data: result
-        });
+        if (result.length > 0) {
+            res.status(200).json({
+                message: 'Mahasiswa Ditemukan',
+                data: result
+            });
+        } else {
+            res.status(200).json({
+                message: 'Mahasiswa Tidak Ditemukan',
+                data: result
+            });
+        }
     })
 })
 
 router.post('/', (req, res, next) => {
-    const mahasiswa = {
-        nim: req.body.nim,
-        nama: req.body.nama
-    }
-    res.status(200).json({
-        message: 'Post Method Mahasiswa',
-        data: mahasiswa
-    });
+    const nim = req.body.nim;
+    const nama = req.body.nama;
+    const jurusan = req.body.jurusan;
+    let sql = `INSERT INTO mahasiswa (nim, nama, jurusan) VALUES ('${nim}', '${nama}', '${jurusan}')`;
+
+    db.query(sql, (err, result) => {
+        if (err) throw err;
+        res.status(200).json({
+            message: 'Berhasil Tambah Data Mahasiswa',
+        });
+    })
 });
 
 router.get('/:nim', (req, res, next) => {
@@ -37,14 +48,28 @@ router.get('/:nim', (req, res, next) => {
 })
 
 router.put('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'Put Method Mahasiswa',
-    });
-});
-router.delete('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'Delete Method Mahasiswa',
-    });
+    const nim = req.body.nim;
+    const nama = req.body.nama;
+    const jurusan = req.body.jurusan;
+    let sql = `UPDATE mahasiswa SET nama = '${nama}', jurusan = '${jurusan}' WHERE nim = '${nim}'`;
+
+    db.query(sql, (err, result) => {
+        if (err) throw err;
+        res.status(200).json({
+            message: 'Berhasil Ubah Data Mahasiswa',
+        });
+    })
+});   
+router.delete('/:nim', (req, res, next) => {
+    const nim = req.params.nim;
+    let sql = `DELETE FROM mahasiswa WHERE nim = '${nim}'`;
+
+    db.query(sql, (err, result) => {
+        if (err) throw err;
+        res.status(200).json({
+            message: 'Berhasil Hapus Data Mahasiswa',
+        });
+    })
 });
 
 
