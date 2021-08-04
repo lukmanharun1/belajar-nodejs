a57b6f9e09baca41550dc4d25c2dc543ae3235d9
fajriputra <fajriwn27@gmail.com>
2021-08-04 10:32:22 +0700

    install nodemon body-parser morgan

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const mahasiswaRoutes = require('./routes/mahasiswa');
 
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json());
 
 app.use('/mahasiswa', mahasiswaRoutes);
 module.exports = app;
--- routes/mahasiswa.js
+++ routes/mahasiswa.js
@@ -8,8 +8,13 @@ router.get('/', (req, res, next) => {
 })
 
 router.post('/', (req, res, next) => {
+    const mahasiswa = {
+        nim: req.body.nim,
+        nama: req.body.nama
+    }
     res.status(200).json({
         message: 'Post Method Mahasiswa',
+        data: mahasiswa
     });
 });
 
